785d39976fe0ac7fcd938cce7b8ad8dbd24b942f
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-24 19:42:50 +0000

    6-size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - Prints Write a C program that prints the size of various types on the computer it is compiled and run on.
- *
+/*
+ * main - Prints prints the size of various types
  * Return: Always 0.
  */
 
