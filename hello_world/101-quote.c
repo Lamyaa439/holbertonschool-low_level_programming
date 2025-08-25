24464a908c75a62d081e92607c6c769fb05a4364
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-25 18:43:03 +0000

    fix 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 /**
 * main - Entry point
 * Return: Always
 **/
 int main(void)
 {
-const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, x, strlen(x));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
