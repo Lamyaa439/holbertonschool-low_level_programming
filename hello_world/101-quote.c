9b2136d597a82077fa168d19d2f58a786168e27f
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-25 11:24:11 +0000

    n

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); /* Error messages go to stderr */
+
+return (1);
+}
+
+
