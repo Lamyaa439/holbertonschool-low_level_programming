8b08dc72538bee11da3ddbb9f800adf5e26e154d
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-25 12:03:46 +0000

    q

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-echo("prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
