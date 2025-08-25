15d8220a7601c39ac60b88a621726eaa007f6391
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-25 11:39:09 +0000

    q

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); /* Error messages go to stderr */
+
+char str[] = "prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
+fflush(stdout);
 
 return (1);
 }
