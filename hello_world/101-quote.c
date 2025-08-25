1eb089aec9592751fc72e95f09e462f225bbd8e0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-25 11:54:20 +0000

    q

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,10 +9,7 @@
 int main(void)
 {
 
-char str[] = "prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
-fflush(stdout);
+echo("prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
