c0c75a55fd1a807fd3acf78c40ea7d7fafc5ee3b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-26 08:17:17 +0000

    fix the file 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/*
- *
- * main - prints the alphabet in lowercase, followed by a new line.
- **/
+/**
+* main - prints the alphabet in lowercase.
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	char alp = 'a';
