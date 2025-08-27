f691c303fc8923af7b886096e7f8d3ee104b8108
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-27 13:12:54 +0000

    add file: 100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Prints all hexadecimal digits in lowercase.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (x = '1'; x <= '9'; x++)
+		{
+			putchar(n);
+			putchar(x);
+			if (x < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
