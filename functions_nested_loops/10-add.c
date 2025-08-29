c84c9241ea2747941909866d036af4d769d81c63
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-08-29 11:51:39 +0000

    add files: 10-main.c, 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result
+* @n: first integer
+* @m: second integer
+*
+* Return: sum of a and b
+*/
+
+int add(int n, int m)
+{
+	int add;
+
+	add = n + m;
+	return (add);
+}
