8e7e1465e88693a766dcd330b03dda524c2848a0
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-09-08 21:56:08 +0000

    create file 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: contains the number of arguments passed to the program.
+ * @argv: is a one-dimensional array of strings. Each string is
+ * one of the arguments that was passed to the program.
+ * Return: 0 SUCCFFEL.
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
