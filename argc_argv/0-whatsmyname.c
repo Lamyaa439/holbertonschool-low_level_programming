b3f5537f8aeec34cc648319e472cc14938b8ec95
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-09-08 21:32:29 +0000

    fix the file 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main(int argc, char* argv[])
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: contains the number of arguments passed to the program.
+ * @argv: is a one-dimensional array of strings. Each string is
+ * one of the arguments that was passed to the program.
+ * Return: program name.
+ **/
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
