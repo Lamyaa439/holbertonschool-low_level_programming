54819856d46b2bc7d3c4a8fe080f768abdcbfc56
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-09-13 10:50:58 +0000

    create new fils

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned int
+ *
+ * Return: pointer to dest
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		src[i] = dest[i];
+		n -= 1;
+	}
+	return (dest);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string.
+ * @s: pointer
+ * @C: char
+ *
+ * Return: a pointer to the first occurrence of
+ * the character c in s, NULL if c not found.
+ **/
+
+char *_strchr(char *s, char c)
+{
+
+	while (s[0] != '\0')
+	{
+		if (s[0] == c)
+			return (s);
+		else if (s[1] == c)
+			return (s + 1);
+		s++;
+	}
+	return (s + 1);
+}
