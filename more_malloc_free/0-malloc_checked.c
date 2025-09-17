41dce77c971c61324d9b2a3080028b37b1141312
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-09-17 10:29:19 +0000

    create files

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc. 
+ * @b: unsigned int
+ *
+ * Return: void
+ *
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	
+
+}
