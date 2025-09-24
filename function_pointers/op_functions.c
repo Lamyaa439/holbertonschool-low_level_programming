0fe8c3d26136c3b2703a806435aac15f77202328
Lamya439 <lamyaaalghaihab@gmail.com>
2025-09-24 13:58:28 +0000

    create a new files

---
+++ function_pointers/op_functions.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+/**
+ * op_add - sum operater
+ * @a: an integer
+ * @b: an integer
+ * Return: sum of a and b
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub operater function
+ * @a: an integer
+ * @b: an integer
+ * Return: the difference of a and b
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multply function
+ * @a: an integer
+ * @b: an integer
+ * Return: the product of a and b.
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division function
+ * @a: an integer
+ * @b: an integer
+ * Return: result of the division of a by b.
+ **/
+int op_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(100);
+
+	}
+}
+/**
+ * op_mod - remainder of the division of a by b
+ * @a: an integer
+ * @b: an integer
+ * Return: the remainder of the division of a by b
+ **/
+int op_mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(100);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
