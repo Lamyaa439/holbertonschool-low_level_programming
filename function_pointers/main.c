5b3085409b32a3825d5a0012181eaa311d9ee2ff
Lamya439 <lamyaaalghaihab@gmail.com>
2025-09-24 14:49:38 +0000

    create a file

---
+++ function_pointers/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "calc.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ **/
+int main(int argc, char **argv)
+{
+	int (*get_op_func(char *s))(int, int), num1, num2;
+
+
+}
