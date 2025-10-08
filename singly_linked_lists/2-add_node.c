610aa06eb0209c8b55b36bce7c7098bb3c9e203b
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-08 08:24:12 +0000

    made some change in 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,6 +4,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: A pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+	
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	
+	return len;
+}
+/**
  * *add_node -  adds a new node at the beginning of a list_t list.
  * @head: ...
  * @str: ...
@@ -25,7 +42,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 
 	(*head) = new_node;
