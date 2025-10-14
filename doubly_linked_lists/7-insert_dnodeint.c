164e4c1b7bc0437f1ebffb81f1bd17bed94d854e
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-14 09:53:18 +0000

    made some change in 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr = *h;
 	unsigned int i;
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (*h == NULL)
+		*h = new_node;
+	if (idx == 0)
 	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 	}
 	/*Insert at beginning*/
@@ -32,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	/*Traverse the list to find the node before*/
-	for (i = 1; i < idx - 1 && curr != NULL; ++i)
+	for (i = 0; i < idx - 1 && curr != NULL; i++)
 		curr = curr->next;
 	/*if position is out of bounds*/
 	if (curr == NULL)
@@ -45,8 +53,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = curr;
 	/*set the next of new node to the next of curr*/
 	new_node->next = curr->next;
-	/*update the next of curr node to new node*/
-	curr->next = new_node;
+
 	/**
 	 * if the new node is not the last node,
 	 * update the prev of next node to the new node
@@ -54,5 +61,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node->next != NULL)
 		new_node->next->prev = new_node;
 
+	curr->next = new_node;
 	return (new_node);
 }
