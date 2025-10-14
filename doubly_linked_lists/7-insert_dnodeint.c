8f41381efafe5f0799c646c55a1ccd5ca09d5dfb
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-14 09:28:16 +0000

    create 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * *insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: the pointer to
+ * @idx: the position to inserts a new node
+ * @n: data
+ * Return: the address of the new node, or NULL if it failed
+ **/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *curr = head;
+
+	/*Insert at beginning*/
+	if (idx == 1)
+	{
+		new_node->next = head;
+		if (head != NULL)
+			head->prev = new_node;
+		head = new_node;
+	}
+
+	/*Traverse the list to find the node before*/
+	for (int i = 1; i < idx - 1 && curr != NULL; ++i)
+		curr = curr->next;
+	/*if position is out of bounds*/
+	if (curr == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/*set the prev of new node to curr*/
+	new_node->prev = curr;
+	/*set the next of new node to the next of curr*/
+	new_node->next = curr->next;
+	/*update the next of curr node to new node*/
+	curr->next = new_node;
+	/**
+	 * if the new node is not the last node,
+	 * update the prev of next node to the new node
+	 **/
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+
+	return (new_node);
+}
