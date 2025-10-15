73fbfae7fef51e74b7d3d9a1298c18db77b229e1
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-15 10:18:46 +0000

    creat  1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure.
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_EXCL | O_RDWR | O_TRUNC, S_IREAD | S_IWRITE);
+	if (fd < 0)
+		return (-1);
+
+	w = write(fd, text_content, strlen(text_content));
+	if (w < 0)
+		close(fd);
+	close(fd);
+	return (1);
+}
