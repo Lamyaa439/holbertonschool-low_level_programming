eea49c0765fe475f2fa43e725e1e47b84ca6c270
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-16 08:37:27 +0000

    update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	while (text_content && text_content[len])
