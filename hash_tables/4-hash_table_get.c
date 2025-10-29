233232755809c3c3a772ac8118e9f2a1e6eb6e00
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-29 10:28:59 +0000

    create 4-hash_table_get.c and 5-hash_table_print.c file

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hash table to look into
+ * @key: the key to looking for
+ *
+ * Return: Returns the value associated with the element
+ * or NULL if key could not be found
+ **/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *curr;
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	curr = ht->array[index];
+
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+		curr = curr->next;
+	}
+	return (NULL);
+}
