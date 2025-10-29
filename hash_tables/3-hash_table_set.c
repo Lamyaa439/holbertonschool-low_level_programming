66d81a07f6d7afa4f4cc8899de4dd990d28192eb
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-29 09:36:05 +0000

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,14 +12,43 @@
  **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash_value = hash_djb2((const unsigned char *)key);
-	unsigned long int index = hash_value % ht->size;
+	hash_node_t *new_node, *curr;
+	unsigned long int index;
 
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+
+	curr = ht->array[index];
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			free(curr->value);
+			curr->value = strdup(value);
+			if (curr->value == NULL)
+				return (0);
+			return (1);
+		}
+		curr = curr->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
-
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
