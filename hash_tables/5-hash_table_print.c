54fb069d287b79dbb42e0571411529dd7d470752
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-10-30 06:32:33 +0000

    Update 5-hash_table_print.c file

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,5 +6,29 @@
  **/
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *curr;
+	int first = 1;
 
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr != NULL)
+		{
+			if (first)
+			{
+				printf("'%s': '%s'", curr->key, curr->value);
+				first = 0;
+			}
+			else
+			{
+				printf(", '%s': '%s'", curr->key, curr->value);
+			}
+			curr = curr->next;
+		}
+	}
+	printf("}\n");
 }
